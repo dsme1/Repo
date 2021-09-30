947163d1f89717d79a36419e99d7e8127b1d559e
Daan Smets <60960402+dsme1@users.noreply.github.com>
2021-09-30 13:53:57 +0200

    Saving after school

--- Losse Opdrachten/Brackets/Program.cs
+++ Losse Opdrachten/Brackets/Program.cs
@@ -16,6 +16,7 @@ namespace Brackets
             Console.WriteLine("Voer een set brackets in die in balans is. Andere tekens of brackets die niet in balans zijn zullen niet worden geaccepteerd.");
             userInput = Console.ReadLine().ToCharArray();
 
+            //takes the user input and checks to see if there are any other characters in it beside brackets. Afterwards it checks to see if brackets are balanced.
             if (HasLetters(userInput))
             {
                 Console.WriteLine("Programma sluit af, u houdt zich niet aan de regels.");
@@ -31,6 +32,12 @@ namespace Brackets
 
         }
 
+        /// <summary>
+        /// Checks to see if brackets are a matching pair in the stack in BracketBalance func
+        /// </summary>
+        /// <param name="charOne"> First bracket put into the stack </param>
+        /// <param name="charTwo"> Second bracket put into the stack </param>
+        /// <returns> Boolean to BracketBalance func </returns>
         static bool MatchingPair(char charOne, char charTwo)
         {
             if (charOne == '(' && charTwo == ')')
@@ -49,26 +56,47 @@ namespace Brackets
                 return false;
         }
 
+        /// <summary>
+        /// Checks if there are any other characters than brackets in users input
+        /// </summary>
+        /// <param name="userInput"> array of chars from users input </param>
+        /// <returns> Boolean to main if else statement </returns>
         static bool HasLetters(char[] userInput)
         {
+            char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*-_=+:;<>,.?/".ToCharArray();
             bool hasLetter = true;
+
+            string userString = userInput.ToString();
+            userString = userString.ToUpper();
+            userInput = userString.ToCharArray();
+
             for (int i = 0; i < userInput.Length; i++)
             {
-                if (userInput[i] != '{' || userInput[i] != '(' || userInput[i] != '[')
+                for (int j = 0; j < alpha.Length; j++)
                 {
-                    hasLetter = true;
-                    break;
-                }else
-                    hasLetter = false;
+                    if (userInput[i] == alpha[j])
+                    {
+                        hasLetter = true;
+                        break;
+                    }
+                    else
+                        hasLetter = false;
+                }                
             }
-
             return hasLetter;
         }
 
+        /// <summary>
+        /// Checks to see wether brackets in users input are balanced against eachother using Stack datatype
+        /// </summary>
+        /// <param name="userInput"> array of chars from users input </param>
+        /// <returns> Boolean for main if else statement </returns>
         static bool BracketBalance(char[] userInput)
         {
+            //empty stack for storing characters
             Stack<char> str = new Stack<char>();
 
+            //iterates over userinput and matches brackets using MatchingPair()
             for (int i= 0; i < userInput.Length; i++)
             {
                 if (userInput[i] == '{' || userInput[i] == '(' || userInput[i] == '[')
