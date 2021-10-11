8ef4784c963e893d6c7793f62f3fedef18183690
Daan <daan.smets1@gmail.com>
2021-10-11 17:06:13 +0200

    Decimal combinations finished
    Made a quick decimal combinations assignment

---
+++ Losse Opdrachten/Decimal Combinations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decimal_Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine(i + "" + j + ",");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
