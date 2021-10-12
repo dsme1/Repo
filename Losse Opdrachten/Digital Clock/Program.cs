399cf88e6697fd1d00df2f1d3045f621e0e44b10
Daan Smets <60960402+dsme1@users.noreply.github.com>
2021-10-12 11:39:35 +0200

    Finished assignment
    Made a program for determining even and uneven numbers.

---
+++ Losse Opdrachten/Digital Clock/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Digital_Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Program PC = new Program();
+            ThreadStart TS = new ThreadStart(PC.ShowTime);
+            Thread t = new Thread(TS);
+            t.Start();
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Calculates current time and keeps rewriting the string indefinitely with a thread timer set to 1 second.
+        /// </summary>
+        public void ShowTime()
+        {
+            for (; ; )
+            {
+                Console.WriteLine(DateTime.Now.ToString());
+                Console.WriteLine("\a");
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+
+        }
+    }
+}
---
+++ Losse Opdrachten/Uneven Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uneven_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            bool stop = false;
+
+            //userinput loop that determines wether the number given by user is even or uneven using the modulo of 10 to get the last digit of a number
+            while (stop == false)
+            {
+                Console.WriteLine("Voer een getal in om te kijken of deze even of oneven is.");
+                userInput = Console.ReadLine();
+
+                if (userInput != "stop")
+                {
+                    int userValue = Int32.Parse(userInput);
+                    userValue = userValue % 10;
+
+                    if (userValue == 1 || userValue == 3 || userValue == 5 || userValue == 7 || userValue == 9)
+                        Console.WriteLine("Uw getal is oneven.");
+                    else
+                        Console.WriteLine("Uw getal is niet oneven.");
+                }
+                else if (userInput == "stop")
+                    stop = true;
+                else
+                    Console.WriteLine("U moet een nummer invoeren.");                
+            }
+
+            if (stop == true)
+                Environment.Exit(0);
+        }
+    }
+}
